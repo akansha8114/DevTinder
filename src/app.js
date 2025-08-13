f842f4521fa453f1f8a05c7a8923e338658c0356
akansha8114 <akanshavgk2004@gmail.com>
2025-08-14 01:03:34 +0530

    created different API

--- src/app.js
+++ src/app.js
@@ -19,6 +19,52 @@ app.post("/signup", async (req,res)=>{
     
 });
 
+//Try to find a user by email : Using a get API
+app.get("/user", async(req,res)=>{
+    const userEmail = req.body.email;
+    try{
+        const user = await User.find({email : userEmail});
+        res.send(user);
+    }catch(err){
+        res.status(400).send("User not found");
+    }
+
+});
+
+//Feed API to get all users on the feed of the user app to swipe left or right
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await User.find({});  //This will return all users in the database
+        res.send(users);
+    }catch(err){
+        res.status(500).send("Error fetching users");
+    }
+});
+
+//Deleting a user by ID
+app.delete("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully");
+    }catch(err){
+        res.status(500).send("Error deleting user");
+    }
+});
+
+//Updating data of a user
+app.patch("/user", async(req,res)=>{
+    const userId = req.body.userId;
+    const updateData = req.body; // Assuming updateData contains the fields to be updated
+    try{
+        const user = await User.findByIdAndUpdate({_id:userId}, updateData, );
+        res.send("User updated successfully");
+    }catch(err){
+        res.status(500).send("Error updating user");
+    }
+});
+
+ 
 
 connectDB()
   .then(() =>{
